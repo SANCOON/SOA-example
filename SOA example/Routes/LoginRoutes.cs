0681439d890a89c6fbff387158f6879c159f6b29
Wilson Zhou <c3306092@uon.edu.au>
2025-04-04 11:44:43 +1100

    Add project files.

---
+++ SOA example/Routes/LoginRoutes.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SOA_example.Routes
+{
+    public static class LoginRoutes
+    {
+        public static void MapLoginRoutes(this WebApplication app)
+        {
+
+        }
+    }
+}
